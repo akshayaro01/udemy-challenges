ae6c82b9834e960172caa63bc17cdb7b54da5d82
akshayaro01 <65898072+akshayaro01@users.noreply.github.com>
2022-01-15 21:50:07 +0530

    Add files via upload
    changes from mac

--- codingchallenge4.js
+++ codingchallenge4.js
@@ -1,26 +1,5 @@
-const d = new Date();
-let dayNo = d.getDay(); // day no.
+const bill = 20;
+const tip= bill<=300 && bill>=50 ? bill*15/100 : bill*20/100;
+console.log("bill:"+bill , "tip:"+tip,bill+tip);
 
-const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; //weekday[3]
-let day = weekday[dayNo]
-switch (day) {
-    case 'Monday':
-        console.log("do programing")
-        break;
-    case 'Tuesday':
-        console.log("do logical thinking")
-        break;
-    case 'Wednesday':
-    case 'Thursday':
-        console.log("rest day")
-        break;
-    case 'Friday':
-        console.log("designing day");
-        break;
-    case 'Saturday':
-        console.log("editing day");
-        break;
-    case 'Sunday':
-        console.log("upload dauy");
-        break;
-}
+
---
+++ ifelsecode.js
@@ -0,0 +1,18 @@
+const day = 'monday';
+
+if (day === 'monday')
+{console.log("programing day");}
+else if (day === 'tuesday')
+{console.log("logical thinking");}
+else if (day === 'wednesday'|| day === 'thursday')
+{console.log("rest day");}
+else if (day === 'friday')
+{console.log("designing day");}
+else if (day === 'saturday')
+{console.log("editing day");}
+else if (day === 'sunday')
+{console.log("upload");}
+else{
+    console.log("not a valid day!");
+}
+
