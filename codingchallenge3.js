ae0a03f15370ef6091d12e3c31f5ffdf2e6cedda
Akshay Arora <aroraakshay993@gmail.com>
2022-01-15 15:18:28 +0530

    first commit

---
+++ codingchallenge3.js
@@ -0,0 +1,26 @@
+const dolphinsScore1 = 97;
+const dolphinsScore2 = 112;
+const dolphinsScore3 = 101;
+
+const koalasScore1 = 109;
+const koalasScore2 = 95;
+const koalasScore3 = 106;
+
+
+const dolphinsAverage = (dolphinsScore1 + dolphinsScore2 + dolphinsScore3) / 3;
+const koalasAverage = (koalasScore1 + koalasScore2 + koalasScore3) / 3;
+if (dolphinsAverage === koalasAverage) {
+    console.log("drawkoalas average is "+koalasAverage+" dolphin average is "+dolphinsAverage)
+}
+else if (dolphinsAverage > koalasAverage && dolphinsAverage >= 100) {
+    console.log("dolphin win dolphin score is "+dolphinsAverage)
+}
+else if
+    (koalasAverage > dolphinsAverage && koalasAverage >= 100) {
+
+    console.log("koalas win koalas score is "+koalasAverage)
+} 
+else
+{
+    console.log("average is not greater then 100 koalas average is "+koalasAverage+" dolphin average is "+dolphinsAverage)
+}
---
+++ codingchallenge4.js
@@ -0,0 +1,26 @@
+const d = new Date();
+let dayNo = d.getDay(); // day no.
+
+const weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; //weekday[3]
+let day = weekday[dayNo]
+switch (day) {
+    case 'Monday':
+        console.log("do programing")
+        break;
+    case 'Tuesday':
+        console.log("do logical thinking")
+        break;
+    case 'Wednesday':
+    case 'Thursday':
+        console.log("rest day")
+        break;
+    case 'Friday':
+        console.log("designing day");
+        break;
+    case 'Saturday':
+        console.log("editing day");
+        break;
+    case 'Sunday':
+        console.log("upload dauy");
+        break;
+}
